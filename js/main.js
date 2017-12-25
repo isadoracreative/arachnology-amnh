756561767552ca6f652ab2039233efc927462d84
Isadora Williams <isadora.williams@gmail.com>
2017-12-25 15:37:43 -0500

    removed .js asynchronous loading of typekit fonts

--- js/main.js
+++ js/main.js
@@ -1,16 +1,6 @@
 /* Script for AMNH scorpions website */
 /* Website visual design and front-end development by Isadora Williams, copyright 2018 */
 
-// asynchronous loading of typekit fonts
-  (function(d) {
-    var config = {
-      kitId: 'upo7xbg',
-      scriptTimeout: 3000,
-      async: true
-    },
-    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-  })(document);
-
 // on scroll, remove class .bg-trans from #nav-header and add class .bg-dark, transition .5s
 // ^before writing this, add class .bg-trans and remove class .bg-dark to #nav-header in html
 
