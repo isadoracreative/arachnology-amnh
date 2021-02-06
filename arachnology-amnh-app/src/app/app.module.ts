79e5e99d643c40787f90ad7ece2592b037c7da63
Isadora Williams Colmenares <isadora.williams@gmail.com>
2021-02-06 15:22:27 -0500

    Break out footer component

--- arachnology-amnh-app/src/app/app.module.ts
+++ arachnology-amnh-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderHomeComponent } from './header-home/header-home.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { FooterGlobalComponent } from './footer-global/footer-global.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderHomeComponent,
-    MainContentComponent
+    MainContentComponent,
+    FooterGlobalComponent
   ],
   imports: [
     BrowserModule,
