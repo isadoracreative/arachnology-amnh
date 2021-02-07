38cce95db52c199f8bd60ee2e59140b968ff2366
Isadora Williams Colmenares <isadora.williams@gmail.com>
2021-02-06 19:41:42 -0500

    Breakout quicklinks aside component

--- arachnology-amnh-app/src/app/app.module.ts
+++ arachnology-amnh-app/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderHomeComponent } from './header-home/header-home.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { FooterGlobalComponent } from './footer-global/footer-global.component';
+import { AsideQuicklinksComponent } from './aside-quicklinks/aside-quicklinks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderHomeComponent,
     MainContentComponent,
-    FooterGlobalComponent
+    FooterGlobalComponent,
+    AsideQuicklinksComponent
   ],
   imports: [
     BrowserModule,
---
+++ arachnology-amnh-app/src/app/aside-quicklinks/aside-quicklinks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aside-quicklinks',
+  templateUrl: './aside-quicklinks.component.html',
+  styleUrls: ['./aside-quicklinks.component.scss']
+})
+export class AsideQuicklinksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
